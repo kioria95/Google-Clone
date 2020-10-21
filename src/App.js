2e77a748069292e78c2a403cc6b3a124a9ead5ec
kioria95 <kenkioria@gmail.com>
2020-10-21 15:17:42 +0300

    Added React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "./App.css";
 import Home from "./pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <Home />
+      <Router>
+        <Switch>
+          <Route path="/search">
+            
+          </Route>
+
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,32 @@
-import React from 'react'
-import './Home.css'
+import { Avatar } from "@material-ui/core";
+import React from "react";
+import "./Home.css";
+
 
 function Home() {
-    return (
-        <div className="home">
-            <h1>Hello guys</h1>
+  return (
+    <div className="home">
+      <div className="home__header">
+        <div className="home__headerAvatar">
+          <div className="home__headerLeft">
+     
+
+          </div>
+
+          <div className="home__headerRight">
+
+
+            <Avatar />
+          </div>
         </div>
-    )
+      </div>
+
+      <div className="home__body">
+
+
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
