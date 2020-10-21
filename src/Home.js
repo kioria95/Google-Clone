851e1f1f37068815345ee1ea02c5a88664849a6d
kioria95 <kenkioria@gmail.com>
2020-10-21 16:12:59 +0300

    Finished doing Home page

--- src/Home.js
+++ src/Home.js
@@ -29,7 +29,7 @@ function Home() {
         />
 
         <div className="home__inputContainer">
-          <Search/>
+          <Search hideButtons/>
         </div>
       </div>
     </div>
--- src/Search.js
+++ src/Search.js
@@ -3,18 +3,21 @@ import "./Search.css";
 import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
 import { Button } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
 
-function Search() {
+function Search({ hideButtons = false }) {
   const [input, setInput] = useState("");
+  const history = useHistory();
 
   const search = (e) => {
     e.preventDefault();
 
+    history.push("/search");
     setInput("");
   };
 
   return (
-    <div className="search">
+    <form className="search">
       <div className="search__input">
         <SearchIcon className="search__inputIcon" />
         <input
@@ -25,13 +28,22 @@ function Search() {
         <MicIcon />
       </div>
 
-      <div className="search__buttons">
-        <Button onClick={search} variant="outlined">
-          Google Search
-        </Button>
-        <Button variant="outlined">I'm feeling lucky</Button>
-      </div>
-    </div>
+      {!hideButtons ? (
+        <div className="search__buttons">
+          <Button onClick={search} variant="outlined" type="submit">
+            Google Search
+          </Button>
+          <Button variant="outlined">I'm feeling lucky</Button>
+        </div>
+      ) : (
+        <div className="search__buttons">
+          <Button className="search__buttonsHidden" onClick={search} variant="outlined" type="submit">
+            Google Search
+          </Button>
+          <Button className="search__buttonsHidden" variant="outlined">I'm feeling lucky</Button>
+        </div>
+      )}
+    </form>
   );
 }
 
